cf31781dc7484f70cad0076cfd7670c909357980
kayxean <36327242+kayxean@users.noreply.github.com>
2025-11-22 23:58:40 +0700

    chore: initial project (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import react from "@vitejs/plugin-react";
+import stylex from "@stylexjs/unplugin"
 import type { UserConfig } from "vite";
 
 export default {
-  plugins: [react()],
+  plugins: [stylex.vite(), react()],
+  base: "/",
 } satisfies UserConfig;
