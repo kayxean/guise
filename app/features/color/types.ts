63dbc46310ca73be6563161ce662deb754a14c45
kayxean <36327242+kayxean@users.noreply.github.com>
2026-01-02 20:45:54 +0700

    fix: color space types

--- app/features/color/types.ts
+++ app/features/color/types.ts
@@ -1,15 +1,17 @@
 type ColorKind = 'rgb' | 'hsl' | 'hsv' | 'hwb' | 'lab' | 'lch' | 'lrgb' | 'oklab' | 'oklch' | 'xyz50' | 'xyz65';
 
+export type ColorMode = Exclude<ColorKind, 'hsv' | 'lrgb' | 'xyz50' | 'xyz65'>;
+
+export type ColorHue = Exclude<ColorMode, 'rgb' | 'lab' | 'oklab'>;
+
 export type ColorValues = [number, number, number];
 
 export type ColorMatrix = [ColorValues, ColorValues, ColorValues];
 
+declare const __mode: unique symbol;
+
 export type ColorSpace<T extends ColorKind> = ColorValues & {
-  readonly type: T;
+  [__mode]: T;
 };
-
-export type ColorMode = Exclude<ColorKind, 'hsv' | 'lrgb' | 'xyz50' | 'xyz65'>;
-
-export type ColorHue = Exclude<ColorMode, 'rgb' | 'lab' | 'oklab'>;
 
 export type ColorFn<T extends ColorKind, X extends Exclude<ColorKind, T>> = (input: ColorSpace<T>) => ColorSpace<X>;
