ff00a0877c330a8a06da4a37943377236e13a499
kayxean <36327242+kayxean@users.noreply.github.com>
2026-01-05 22:12:27 +0700

    test: dynamic styles (#33)

--- app/features/browser/index.tsx
+++ app/features/browser/index.tsx
@@ -1,15 +1,36 @@
+import type { CSSProperties } from 'react';
 import * as stylex from '@stylexjs/stylex';
+import { useColorStore } from './color';
 import { BookmarkBar } from './components/bookmark-bar';
 import { TabList } from './components/tab-list';
 import { TabPanel } from './components/tab-panel';
 import { Toolbar } from './components/toolbar';
-import { Theme } from './theme';
 import { colors } from './tokens.stylex';
 
+type ColorTokens = typeof colors;
+type ColorKey = keyof ColorTokens;
+
+const getVarName = (stylexVar: string) => stylexVar.replace(/^var\(/, '').replace(/\)$/, '');
+
 export function Browser() {
+  const chrome = useColorStore((state) => state);
+
+  const dynamicStyles = (Object.keys(chrome) as ColorKey[]).reduce(
+    (acc, key) => {
+      const token = colors[key];
+      if (token && typeof token.toString === 'function') {
+        const varName = getVarName(token.toString());
+        acc[varName as keyof CSSProperties] = chrome[key as keyof typeof chrome] as string;
+      }
+      return acc;
+    },
+    {} as Record<string, string>,
+  );
+
+  const stylexProps = stylex.props(browser.layout);
+
   return (
-    <div {...stylex.props(browser.layout)}>
-      <Theme />
+    <div {...stylexProps} style={{ ...stylexProps.style, ...dynamicStyles }}>
       <div {...stylex.props(browser.navigation)}>
         <TabList />
         <Toolbar />
