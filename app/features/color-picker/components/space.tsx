385597d3446caa74554a65c0376133277a300f20
kayxean <36327242+kayxean@users.noreply.github.com>
2026-03-19 22:22:14 +0700

    chore: add active color space

--- app/features/color-picker/components/space.tsx
+++ app/features/color-picker/components/space.tsx
@@ -1,23 +1,59 @@
-import type { ColorMode } from '@kayxean/chromatrix/types';
+import type { ColorMode, ColorSpace } from '@kayxean/chromatrix/types';
+import * as stylex from '@stylexjs/stylex';
 
 const ALL_SPACES: ColorMode[] = ['hex', 'rgb', 'hsl', 'hwb', 'lab', 'lch', 'oklab', 'oklch'];
 
 export function SpacePicker({
-  onSelect,
+  currentSpace,
   allowedMode,
+  useHex = false,
+  onSelect,
 }: {
-  onSelect: (space: ColorMode) => void;
+  currentSpace: ColorSpace;
   allowedMode?: ColorMode[];
+  useHex?: boolean;
+  onSelect: (space: ColorMode) => void;
 }) {
   const activeMode = allowedMode ? ALL_SPACES.filter((s) => allowedMode.includes(s)) : ALL_SPACES;
 
+  const activeSpace = useHex ? 'hex' : currentSpace;
+
   return (
-    <div>
+    <div {...stylex.props(styles.layout)}>
       {activeMode.map((space) => (
-        <button type="button" key={space} onClick={() => onSelect(space)}>
+        <button
+          type="button"
+          key={space}
+          onClick={() => onSelect(space)}
+          {...stylex.props(styles.button, space === activeSpace ? styles.btnActive : styles.btn)}>
           {space}
         </button>
       ))}
     </div>
   );
 }
+
+const styles = stylex.create({
+  layout: {
+    display: 'grid',
+    gap:'.75rem',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+  },
+  button: {
+    alignItems: 'center',
+    borderRadius: '0.25rem',
+    cursor: 'pointer',
+    display: 'inline-flex',
+    fontSize: '0.75rem',
+    justifyContent: 'center',
+    padding: '0.5rem 1rem',
+  },
+  btn: {
+    backgroundColor: '#323232',
+    color: '#f2f3f4',
+  },
+  btnActive: {
+    backgroundColor: '#f2f3f4',
+    color: '#323232',
+  },
+});
